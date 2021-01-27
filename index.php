eedcfc95d79fbfe2159aa86892445a86d352e306
Kurniawanas17 <77954497+Kurniawanas17@users.noreply.github.com>
2021-01-27 14:56:32 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html>
+<!-- Bootstrap core CSS -->
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<head>
+	<title>BIODATA DIRI</title>
+</head>
+<body background="bg.jpg">
+<h1 align="center">TUGAS 1 . [ WEB SEMANTIK ]</h1>
+<hr>
+<br>
+<table width="745" border="0" cellpadding="5" cellspacing="0" align="center">
+	<tr align="center" bgcolor="gray">
+		<td width="175" colspan="4">DATA DIRI</td>
+	</tr>
+	<tr>
+		<td>Nama</td>
+		<td>:</td>
+		<td>KURNIAWAN AS</td>
+		<td rowspan="10" align="center"><img src="1.jpg" width="200" height="250">
+		</td>
+	</tr>
+	<tr>
+		<td>Nim</td>
+		<td>:</td>
+		<td>217 280 064</td>
+	</tr>
+	<tr>
+		<td>Kelas</td>
+		<td>:</td>
+		<td>B</td>
+	</tr>
+	<tr>
+		<td>Jurusan</td>
+		<td>:</td>
+		<td>INFORMATIKA</td>
+	</tr>
+	<tr>
+		<td>Tempat, Tanggal Lahir</td>
+		<td>:</td>
+		<td>PINRANG, 01 JULI 1999</td>
+	</tr>
+	<tr>
+		<td>Jenis Kelamin</td>
+		<td>:</td>
+		<td>LAKI LAKI</td>
+	</tr>
+	<tr>
+		<td>Alamat</td>
+		<td>:</td>
+		<td>TIROANG</td>
+	</tr>
+		<tr>
+		<td>Mata Kuliah</td>
+		<td>:</td>
+	</tr>
+<table width="745" border="1" cellpadding="5" cellspacing="0" align="center">
+	<tr align="center" bgcolor="gray">
+		<td width="250">MATA KULIAH</td>
+		<td width="70">KODE</td>
+		<td width="100">HARI</td>
+		<td width="100">JAM</td>
+		<td width="35">SKS</td>
+		>
+	</tr>
+	<tr>
+		<td>Tata Tulis dan Presentasi</td>
+		<td align="center">MKK 2826</td>
+		<td align="center">Senin</td>
+		<td align="center">09.30 - 10.50</td>
+		<td align="center">2</td>
+	</tr>
+	<tr>
+		<td>Al-Islam Kemuhammadiyahan VI</td>
+		<td align="center">MPK 2806</td>
+		<td align="center">Kamis</td>
+		<td align="center">10.40 - 12.10</td>
+		<td align="center">1</td>
+	</tr>
+	<tr>
+		<td>Rekayasa Perangkat Lunak</td>
+		<td align="center">MKK 2824</td>
+		<td align="center">Kamis</td>
+		<td align="center">13.00 - 15.00</td>
+		<td align="center">3</td>	
+	</tr>
+	<tr>
+		<td>Mobile Computing</td>
+		<td align="center">MKK 2825</td>
+		<td align="center">Jumat</td>
+		<td align="center">07.30 - 09.30</td>
+		<td align="center">3</td>	
+	</tr>
+		<tr>
+		<td>Pemodelan dan Simulasi</td>
+		<td align="center">MKK 2823</td>
+		<td align="center">Jumat</td>
+		<td align="center">15.30 - 17.10</td>
+		<td align="center">2</td>
+	</tr>
+	<tr>
+		<td>Kerja Praktek</td>
+		<td align="center">MPB 2802</td>
+		<td align="center"> - </td>
+		<td align="center"> - </td>
+		<td align="center">2</td>
+	</tr>
+	<tr>
+		<td bgcolor="gray" colspan="5" align="center">MATA KULIAH PILIHAN</td>
+	</tr>
+	<tr>
+		<td>Augmented dan Virtual Reality</td>
+		<td align="center">MKB 2813</td>
+		<td align="center">Senin</td>
+		<td align="center">13.00 - 15.00</td>
+		<td align="center">3</td>	
+	</tr>
+	<tr>
+		<td>Web Semantik</td>
+		<td align="center">MKB 2814</td>
+		<td align="center">Jumat</td>
+		<td align="center">09.30 - 11.30</td>
+		<td align="center">3</td>
+	</tr>
+		<tr>
+		<td bgcolor="gray" colspan="4" align="center">Jumlah SKS</td>
+		<td bgcolor="gray" colspan="1" align="center">19</td>
+	</tr>
+</table>
+</table>
+</body>
+<hr>
+<link rel="stylesheet" type="text/css" href="js/bootstrap.min.js">
+</html>
